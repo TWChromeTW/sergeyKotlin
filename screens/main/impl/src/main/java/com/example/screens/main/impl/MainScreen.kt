b07ad3af2503fe19359406b1fc2577c1c30a567a
chrome <orlov707kostya@gmail.com>
2023-06-23 19:21:25 +0600

    starting the third screen i do main info component

--- screens/main/impl/src/main/java/com/example/screens/main/impl/MainScreen.kt
+++ screens/main/impl/src/main/java/com/example/screens/main/impl/MainScreen.kt
@@ -1,199 +1,140 @@
 package com.example.screens.main.impl
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.impl.R
-import com.example.screens.main.api.data.Player
-import com.example.screens.main.impl.components.MainScreenContent
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 @Composable
-@ExperimentalLayoutApi
-@ExperimentalMaterialApi
-@ExperimentalMaterial3Api
-fun MainScreen(
-    modifier: Modifier = Modifier,
-    players: List<Player>,
-    @DrawableRes
-    placeHolderDrawableRes: Int = R.drawable.dota2_logo_icon
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val scaffoldState = rememberScaffoldState()
-    val lazyColumnState = rememberLazyListState()
-    val firstVisibleItemIndex by remember {
-        derivedStateOf { lazyColumnState.firstVisibleItemIndex }
-    }
-    val textFieldValue = remember {
-        mutableStateOf(
-            TextFieldValue("")
-        )
-    }
-    var isRefreshing by remember { mutableStateOf(false) }
-    var isFabVisible by remember { mutableStateOf(true) }
-    val pullRefreshState = rememberPullRefreshState(
-        refreshing = isRefreshing,
-        onRefresh = {
-            coroutineScope.launch {
-                isRefreshing = true
-                delay(500L)
-                isRefreshing = false
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Search()
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
+            items(count = 20){
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
+                User()
             }
         }
-    )
-
-    MainScreenContent(
-        modifier = modifier,
-        scaffoldState = scaffoldState,
-        searchFieldValue = textFieldValue,
-        pullRefreshState = pullRefreshState,
-        players = players,
-        lazyColumnState = lazyColumnState,
-        placeHolderDrawableRes = placeHolderDrawableRes,
-        isRefreshing = isRefreshing,
-        isFabVisible = isFabVisible,
-        onFabIsClicked = {
-            coroutineScope.launch {
-                lazyColumnState.animateScrollToItem(0)
-                isFabVisible = false
-            }
-        }
-    )
-
-    LaunchedEffect(firstVisibleItemIndex) { isFabVisible = firstVisibleItemIndex >= 3 }
-}
-
-@Composable
-@Preview(
-    showSystemUi = true,
-    device = "id:pixel_6a",
-    group = "MainScreenPreview",
-    name = "EmptyList"
-)
-@ExperimentalLayoutApi
-@ExperimentalMaterial3Api
-@ExperimentalMaterialApi
-fun MainScreenPreview1() {
-    MaterialTheme {
-        MainScreen(
-            modifier = Modifier.fillMaxSize(),
-            players = listOf()
-        )
     }
 }
 
+@Preview
 @Composable
-@Preview(
-    showSystemUi = true,
-    device = "id:pixel_6a",
-    group = "MainScreenPreview",
-    name = "NotEmptyList"
-)
-@ExperimentalLayoutApi
-@ExperimentalMaterial3Api
-@ExperimentalMaterialApi
-fun MainScreenPreview2() {
-    val players = listOf(
-        Player(
-            id = "123",
-            nickname = "Kostya",
-            avatar = null
-        ),
-        Player(
-            id = "1234",
-            nickname = "Leha",
-            avatar = null
-        ),
-        Player(
-            id = "12345",
-            nickname = "Yarik",
-            avatar = null
-        ),
-        Player(
-            id = "123456",
-            nickname = "KonstAntin",
-            avatar = null
-        ),
-        Player(
-            id = "123567",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "12358",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "123569",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235691",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235692",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235693",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235694",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235695",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235696",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235697",
-            nickname = "Alex",
-            avatar = null
-        ),
-        Player(
-            id = "1235698",
-            nickname = "Alex",
-            avatar = null
-        )
-    )
+private fun Search() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Blue)
+            .padding(30.dp, 10.dp, 0.dp, 10.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .clickable(onClick = { TODO() })
+                .background(
+                    Color.Red,
+                    shape = RoundedCornerShape(15.dp)
+                )
+                .padding(30.dp, 10.dp, 175.dp, 10.dp),
+        ) {
+            Text(text = "Search for players")
+        }
+    }
+}
 
+@Preview
+@Composable
+private fun User() {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 7.dp)
+    ){
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { TODO() })
+            .padding(horizontal = 15.dp)
+            .background(
+                Color.LightGray,
+                shape = RoundedCornerShape(25.dp)
+            )
+        ) {
+            Image(painter = painterResource(id = R.drawable.monogram),
+                contentDescription = "image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(10.dp, 5.dp, 0.dp, 5.dp)
+                    .size(64.dp)
+            )
+
+            Column {
+                Text(
+                    text = "Header",
+                    modifier = Modifier
+                        .padding(20.dp, 13.dp, 0.dp, 5.dp)
+                )
+                Text(
+                    text = "Subhead",
+                    modifier = Modifier
+                        .padding(20.dp, 0.dp, 0.dp, 0.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() {
     MaterialTheme {
-        MainScreen(
-            modifier = Modifier.fillMaxSize(),
-            players = players
-        )
+        MainScreen()
     }
 }
---
+++ screens/player/impl/src/main/java/com/example/screens/player/impl/components/Main.kt
@@ -0,0 +1,104 @@
+package com.example.screens.player.impl.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.screens.player.impl.R
+
+@Composable
+internal fun Main() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                Color.White, shape = RoundedCornerShape(25.dp)
+            )
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier.padding(15.dp, 7.dp, 0.dp, 7.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .size(112.dp)
+                        .background(
+                            Color.Blue, shape = RoundedCornerShape(75.dp)
+                        ),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.dota2_icon_placeholder),
+                        contentDescription = "dota_icon",
+                        Modifier.size(80.dp)
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 20.dp, 100.dp, 0.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Column(
+                    modifier = Modifier
+                ) {
+                    Text(text = "NickName")
+                    Text(text = "Chrome")
+                }
+
+                Column(
+                    modifier = Modifier.padding(5.dp, 10.dp, 0.dp, 0.dp)
+                ) {
+                    Text(text = "Last Online")
+                    Text(text = "Never")
+                }
+            }
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp, 10.dp, 15.dp, 0.dp),
+        ) {
+            Text(text = "PROFILE LINK")
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp, 10.dp, 15.dp, 7.dp),
+        ) {
+            Text(text = "STEAM PROFILE LINK")
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun Main_Preview() {
+    Main()
+}
